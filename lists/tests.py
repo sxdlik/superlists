d39449d93b0ea6cf7e0d74bd419c5334944ba0c3
sxd <sxdlik@163.com>
2020-12-18 16:17:22 +0800

    首页视图输出使用了模板，并注册了 lists app

--- lists/tests.py
+++ lists/tests.py
@@ -8,16 +8,7 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     """ 首页测试 """
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """ 测试根路由返回首页的视图函数 """
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        """ 测试首页使用正确的html """
-        request = HttpResponse()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>待办事项列表</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        """ 测试使用 home 模板 """
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<html><title>待办事项列表</title></html>')
+    return render(request, 'home.html')
