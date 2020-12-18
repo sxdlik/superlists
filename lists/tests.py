72a626c8070efd5cd04bc6c7641f1dc0d6077020
sxd <sxdlik@163.com>
2020-12-18 16:00:11 +0800

    第一个单元测试，和根 url 设置，以及简单视图

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
 
-# Create your tests here.
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+    """ 首页测试 """
+
+    def test_root_url_resolves_to_home_page_view(self):
+        """ 测试根路由返回首页的视图函数 """
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
