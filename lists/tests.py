0be2f9a75641be2857bbb6d8c7f31f7c6c4abaa0
sxd <sxdlik@163.com>
2020-12-18 16:06:16 +0800

    首页返回了一个html代码

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpResponse
 
 from lists.views import home_page
 
@@ -11,3 +12,12 @@ class HomePageTest(TestCase):
         """ 测试根路由返回首页的视图函数 """
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """ 测试首页使用正确的html """
+        request = HttpResponse()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>待办事项列表</title>', html)
+        self.assertTrue(html.endswith('</html>'))
