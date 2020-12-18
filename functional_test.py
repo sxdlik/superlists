edfdd9a2386899f369e5039be860525b76965c6d
sxd <sxdlik@163.com>
2020-12-18 17:03:11 +0800

    重构功能测试，把列表显示功能单独提取出来做为一个函数调用

--- functional_test.py
+++ functional_test.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """ 测试可以输入一个待办事项以及显示待办事项列表 """
 
@@ -38,9 +43,7 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 东东学习Django', [row.text for row in rows])
+        self.check_for_row_in_list_table('1： 东东学习Django')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 他输入了 "学习Python"
@@ -50,10 +53,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # 页面再次更新，他的清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 东东学习Django', [row.text for row in rows])
-        self.assertIn('2: 东东学习Python', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: 东东学习Django')
+        self.check_for_row_in_list_table('2: 东东学习Python')
 
         # 东东想知道这个网站是否会记住他的清单
         # 他看到网站为他生成了一个唯一的URL
@@ -63,6 +64,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # 他很满意，去睡觉了
         self.fail('本次测试完成')
+
 
 if __name__ == '__main__':
     unittest.main()
