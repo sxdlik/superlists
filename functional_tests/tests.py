aa3c527bf9e673e2a35f70b2ea2d70f2b4d84d58
sxd <sxdlik@163.com>
2020-12-20 23:33:02 +0800

    修改列表显示函数，改成隐式等待，同时去掉了 sleep 显式等待

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,9 @@ import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -14,10 +17,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """ 测试可以输入一个待办事项以及显示待办事项列表 """
@@ -41,20 +52,18 @@ class NewVisitorTest(LiveServerTestCase):
         # 他按下回车键后，页面更新了
         # 待办事项中显示了 "1：东东学习Django"
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: 东东学习Django')
+        self.wait_for_row_in_list_table('1: 东东学习Django')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 他输入了 "学习Python"
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('东东学习Python')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # 页面再次更新，他的清单中显示了这两个待办事项
-        self.check_for_row_in_list_table('1: 东东学习Django')
-        self.check_for_row_in_list_table('2: 东东学习Python')
+        self.wait_for_row_in_list_table('1: 东东学习Django')
+        self.wait_for_row_in_list_table('2: 东东学习Python')
 
         # 东东想知道这个网站是否会记住他的清单
         # 他看到网站为他生成了一个唯一的URL
