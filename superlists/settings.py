e0c700dd576bafbed8943d58353b37fbfe573b9b
sxd <sxdlik@163.com>
2020-12-22 14:11:46 +0800

    设置测试地址以及设置数据库位置

--- superlists/settings.py
+++ superlists/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'superlists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +77,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../database/db.sqlite3',
     }
 }
 
